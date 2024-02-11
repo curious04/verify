516381e0f48a42399f27181bc720509a63024c82
Hrithik Singh <hrithikks10@gmail.com>
2024-02-12 00:29:10 +0530

    create task

--- components/ui/shared/Footer.tsx
+++ components/ui/shared/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
           />
         </Link>
 
-        <p>2023 Verify. All Rights reserved.</p>
+        <p>2024 Verify. All Rights reserved.</p>
       </div>
     </footer>
   )
